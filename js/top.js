881938437c1eace6c268aff806b9d4be791d303f
Slobberbone <slobberbone4884@gmail.com>
2019-03-21 23:29:36 +0100

    Fix new version alert

--- js/top.js
+++ js/top.js
@@ -13,15 +13,18 @@ function GetLatestRealaseInfo() {
 		}).then(function(lastRealase){
 			latestVersion = lastRealase[0].name;
 			var portalDownloadId = document.getElementById("portal-download");
+			var portalDownloadImgId = document.getElementById("portal-download-img");
 			var portalDownloadLinkId = document.getElementById("portal-download-link");
 			portalDownloadLinkId.href=lastRealase[0].zipball_url;
 			portalDownloadId.style.display="none";
+			portalDownloadImgId.style.display="none";
 			if(!localeVersion){
 				localeVersion = "0.1";
 			}
 			if(latestVersion){
 				if(localeVersion < latestVersion){
 					portalDownloadId.style.display="block";
+					portalDownloadImgId.style.display="block";
 				}
 			}
 		
@@ -31,14 +34,4 @@ function GetLatestRealaseInfo() {
 	$.getJSON( "config/config.sample.json").done(function(json) {
 		compareToGithubVersion(json["version"]);
 	});
-#	$.getJSON( "config/config.json").done(function(json) {
-#		var version = json["version"];
-#		if(!version){
-#			$.getJSON( "config/config.sample.json").done(function(json) {
-#				compareToGithubVersion(json["version"]);
-#			});
-#		} else {
-#			compareToGithubVersion(version);
-#		}
-#	});
 }
